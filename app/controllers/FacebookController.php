00b3f1690cc2c4a5d0b980cda8a535cf6bff8d43
theyhoang <theyhoang@gmail.com>
2014-04-30 23:36:00 -0700

    cleaning up login/register

--- app/controllers/FacebookController.php
+++ app/controllers/FacebookController.php
@@ -38,7 +38,7 @@ class FacebookController extends BaseController{
 
                 $user_profile = $facebook->api('/me','GET');
 
-                return Redirect::to('profile');
+                return Redirect::to('fb_profile');
 
 
             } catch(FacebookApiException $e) {
@@ -58,6 +58,81 @@ class FacebookController extends BaseController{
             echo 'Please <a href="' . $login_url . '">login.</a>';
 
         }
+    }
+
+    public function profile() {
+        $config = Cache::get('config');
+
+        if($config) {
+            echo "<h1>Config was cached</h1>";
+        } else {
+            echo "<h1>Config was Not cached </h1>";
+            $config = array(
+                'appId' => Config::get('facebook.appId'),
+                'secret' => Config::get('facebook.secret'),
+            );
+            Cache::put('config',$config,10);
+        }
+
+
+
+        $facebook = new Facebook($config);
+
+        $user_id = $facebook->getUser();
+        if($user_id) {
+
+            // We have a user ID, so probably a logged in user.
+            // If not, we'll get an exception, which we handle below.
+            try {
+
+                $user_profile = $facebook->api('/me','GET');
+
+
+            } catch(FacebookApiException $e) {
+                // If the user is logged out, you can have a
+                // user ID even though the access token is invalid.
+                // In this case, we'll get an exception, so we'll
+                // just ask the user to login again here.
+                $login_url = $facebook->getLoginUrl();
+                echo 'Please <a href="' . $login_url . '">login.</a>';
+                error_log($e->getType());
+                error_log($e->getMessage());
+            }
+        } else {
+
+            return Redirect::to('login');
+
+        }
+
+        //$user_profile = $facebook->api('/me','GET');
+
+        echo "Name: " . $user_profile['name'] . "<br>";
+        echo "facebook_id: " . $user_profile['id'] . "<br>";
+        echo "hometown: " . $user_profile['hometown']['name'] . "<br>";
+
+        foreach($user_profile["education"] as $education) {
+            if($education["type"] == "High School") {
+                $highschool = $education["school"]["name"];
+                break;
+            }
+        }
+
+        //echo "education: " . var_dump($user_profile["education"]) . "<br>";
+        echo "high school: " . $highschool . "<br>";
+
+        $fb_pic = "http://graph.facebook.com/".$user_profile['id']."/picture?type=large";
+        echo "fb_pic: <a href='" . $fb_pic . "'>facebook_profile_pic</a><br>";
+        echo "user_profile: <br>";
+
+        //var_dump($user_profile);
+        //$params = array( 'next' => 'http://localhost:8000/logout' );
+        //echo "<br><a href='". $facebook->getLogoutUrl($params) . "'>logout</a>";
+
+        $facebook->destroySession();
+
+        return View::make('register',[
+
+        ]);
     }
 
 
--- app/routes.php
+++ app/routes.php
@@ -28,75 +28,7 @@ Route::get('/login','RecruitController@login');
 
 Route::get('/register','RecruitController@register');
 
-Route::get('/tweets', function()
-{
-    $json = Cache::get('twitter-uscitp');
-
-    if (!$json) {
-        $twitterApi = new Tang\TwitterRestApi\TwitterApi([
-            'api_key' => Config::get('twitter.api_key'),
-            'api_secret' => Config::get('twitter.api_secret')
-        ]);
-
-        $json = $twitterApi->authenticate()->get('statuses/user_timeline', [
-            'screen_name' => 'uscitp',
-            'count' => 10,
-            'exclude_replies' => true
-        ]);
-
-        Cache::put('twitter-uscitp', $json, 10);
-    }
-
-    header('Content-type: application/json');
-    echo $json;
-});
-
-Route::get('/profile', function()
-    {
-
-        $config = Cache::get('config');
-
-        if($config) {
-            echo "<h1>Config was cached</h1>";
-        } else {
-            echo "<h1>Config was Not cached </h1>";
-            $config = array(
-                'appId' => Config::get('facebook.appId'),
-                'secret' => Config::get('facebook.secret'),
-            );
-            Cache::put('config',$config,10);
-        }
-
-
-
-   $facebook = new Facebook($config);
-
-    $user_profile = $facebook->api('/me','GET');
-    echo "Name: " . $user_profile['name'] . "<br>";
-    echo "facebook_id: " . $user_profile['id'] . "<br>";
-    echo "hometown: " . $user_profile['hometown']['name'] . "<br>";
-
-    foreach($user_profile["education"] as $education) {
-        if($education["type"] == "High School") {
-            $highschool = $education["school"]["name"];
-            break;
-        }
-    }
-
-    //echo "education: " . var_dump($user_profile["education"]) . "<br>";
-    echo "high school: " . $highschool . "<br>";
-
-    $fb_pic = "http://graph.facebook.com/".$user_profile['id']."/picture?type=large";
-    echo "fb_pic: <a href='" . $fb_pic . "'>facebook_profile_pic</a><br>";
-    echo "user_profile: <br>";
-
-
-    var_dump($user_profile);
-
-   $params = array( 'next' => 'http://localhost:8000/logout' );
-   echo "<br><a href='". $facebook->getLogoutUrl($params) . "'>logout</a>";
-});
-
+Route::get('/fb_profile', "FacebookController@profile");
 
 Route::get('/logout', function() {
 
@@ -113,15 +45,9 @@ Route::get('/logout', function() {
         Cache::put('config',$config,10);
     }
 
-
-
     $facebook = new Facebook($config);
     $facebook->destroySession();
     return Redirect::to('login');
-
-
-
-
 });
 
 
